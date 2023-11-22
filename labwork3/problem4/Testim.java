eba8bea62a889bd7edc052591e566e27878f34bb
zhansu <118667628+zhanius@users.noreply.github.com>
2023-11-22 19:12:06 +0600

    updated

--- labwork3/problem4/Testim.java
+++ labwork3/problem4/Testim.java
@@ -6,24 +6,27 @@ import java.util.Vector;
 
 public class Testim {
     public static void main(String[]args) throws CloneNotSupportedException{
-        Employee johnny = new Employee("Johnny", "JR12", LocalDate.of(2020,12,12),1000.0);
-        Employee kitana = new Employee("Kitana", "BR12", LocalDate.of(2019,12,12),1300.0);
-        Employee sub = new Employee("Subzero", "LR12", LocalDate.of(2019,10,11),1200.0);
+        Employee erkin = new Employee("Erkin", "JR12", LocalDate.of(2020,12,12),1000.0);
+        Employee askar = new Employee("Askar", "BR12", LocalDate.of(2019,12,12),1300.0);
+        Employee lashyn = new Employee("Lashyn", "LR12", LocalDate.of(2019,10,11),1200.0);
 
-        Employee sub1 = (Employee) sub.clone();
-        System.out.println("Cloned: " + sub1);
+        Employee lashyn1 = (Employee) lashyn.clone();
+        System.out.println("Cloned: " + lashyn1);
+        lashyn1.setHireDate(LocalDate.of(2023,12,12));
+        System.out.println("Added new date to cloned one: " + lashyn1);
+        System.out.println(lashyn);
 
         Manager kama = new Manager("Kamazhay", "KR12", LocalDate.of(2017,8,27), 2000.0);
         Manager uldino = new Manager("Uldana", "UR12", LocalDate.of(2017,7,25), 2000.0);
-        kama.addTeammate(johnny);
-        kama.addTeammate(kitana);
-        kama.addTeammate(sub);
+        kama.addTeammate(erkin);
+        kama.addTeammate(askar);
+        kama.addTeammate(lashyn);
 
         Vector<Employee>employees = new Vector<>();
-        employees.add(johnny);
-        employees.add(kitana);
-        employees.add(sub);
-        employees.add(sub1);
+        employees.add(erkin);
+        employees.add(askar);
+        employees.add(lashyn);
+        employees.add(lashyn1);
 
         System.out.println("BEFORE SORTING:");
         employees.forEach(System.out::println);
